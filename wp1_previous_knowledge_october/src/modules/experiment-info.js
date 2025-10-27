940b986f5792a61b6385e249d536a99d73bba366
chvhouwe <80961833+chvhouwe@users.noreply.github.com>
2025-10-27 13:44:39 +0100

    Added via the Open Science Framework

---
+++ wp1_previous_knowledge_october/src/modules/experiment-info.js
@@ -0,0 +1,27 @@
+export const hugginsStimuli = ["HP_1_1", "HP_1_2", "HP_1_3", "HP_2_1", "HP_2_2", "HP_2_3"];
+
+// Patterns that are used for exposure = sets 1 and 2
+
+export const allPatternSets = [
+  [
+    ["Be", "Pa"],
+    ["Gi", "Va"],
+    ["Je", "Wo"],
+    ["Ju", "Mi"],
+    ["Ke", "Ge"],
+    ["Me", "Zu"],
+    ["Ve", "Fe"],
+    ["We", "To"],
+  ],
+  [
+    ["Du", "Jo"],
+    ["Fu", "Di"],
+    ["Le", "Fo"],
+    ["Lu", "Tu"],
+    ["No", "So"],
+    ["Nu", "Bu"],
+    ["Wi", "Vo"],
+    ["Zo", "Pi"],
+  ],
+];
+
